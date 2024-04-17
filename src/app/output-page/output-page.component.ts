bbebf769d98052b1e847709d09551819294bd00a
Navdeep Kaur <kau15187@sheridancollege.ca>
2024-04-16 20:32:20 -0400

    output-page

--- src/app/output-page/output-page.component.ts
+++ src/app/output-page/output-page.component.ts
@@ -1,3 +1,4 @@
+//ouput ts
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TipCalculationService } from '../tip-calculation.service';
